54440569aacb555d188a39d3c3f65f1da6f3fec7
badsha103868 <badshagolder5@gmail.com>
2025-08-29 03:38:31 +0600

    added heart icon funtion and some call button funtion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
 // 
+// heart icon click funtion
+const hearts = document.getElementsByClassName("heart-point")
+ let heartBalance = 0;
+for(const heart of hearts){
+  heart.addEventListener("click", function(){
+    heartBalance++;
+   const availableHeartPoint = document.getElementById("heart-balance");
+    availableHeartPoint.innerText = heartBalance;
+  })
+}
+
+// call button click funtion
+
+
+
+
+
+
+const callButtons = document.getElementsByClassName("call-button")
+// console.log(callButtons)
+  let coins = 100;
+for(const callButton of callButtons){
+  callButton.addEventListener("click", function(){
+    const card = callButton.parentNode.parentNode;
+    // console.log(card)
+    const serviceName = card.querySelector(".title").innerText;
+    const number = card.querySelector(".number").innerText;
+    alert(`☎ Calling ${serviceName} ${number}`);
+    
+    
+
+    if(coins >= 20){
+      coins-=20
+      const coinBalance = document.getElementById("coin-balance");
+      coinBalance.innerText = coins;
+      return;
+    }
+    else{
+      alert("❌ আপনার পর্যাপ্ত পরিমাণ কয়েন নাই! কল করতে কমপক্ষে ২০ কয়েন লাগবে।")
+    }
+    
+  })
+}
